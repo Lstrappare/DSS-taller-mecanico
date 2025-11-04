efdee39c7d3eceaffe8baafa6b403af382351c59
Lstrappare <j.m.cisval1@gmail.com>
2025-11-03 20:11:54 -0600

    Initial Commit

---
+++ DSS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DSS
+//
+//  Created by Jose Cisneros on 03/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DSS/DSSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DSSApp.swift
+//  DSS
+//
+//  Created by Jose Cisneros on 03/11/25.
+//
+
+import SwiftUI
+
+@main
+struct DSSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
